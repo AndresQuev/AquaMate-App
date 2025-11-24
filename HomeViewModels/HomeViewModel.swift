3284bdd02c2b6aa2a09ae4fb83f70cabf7625eaa
AndresQuev <andresquev4@gmail.com>
2025-11-24 16:48:59 -0300

    Home View Prototipe (just views, not logic yet)

---
+++ HomeViewModels/HomeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewModel.swift
+//  AquaMate
+//
+//  Created by Andrés Quevedo on 24/11/2025.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewModel()
+}
