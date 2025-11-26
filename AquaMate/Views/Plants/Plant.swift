66541091bc92c844574dbe4e5946d36c2fd31a96
AndresQuev <andresquev4@gmail.com>
2025-11-26 09:53:05 -0300

    New My Plants Section

--- AquaMate/Views/Plants/Plant.swift
+++ AquaMate/Views/Plants/Plant.swift
@@ -1,18 +1,18 @@
 //
-//  HomeView.swift
+//  Plant.swift
 //  AquaMate
 //
-//  Created by Andrés Quevedo on 27/10/2025.
+//  Created by Andrés Quevedo on 26/11/2025.
 //
 
 import SwiftUI
 
-struct HomeView: View {
+struct Plant: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    HomeView()
+    Plant()
 }
