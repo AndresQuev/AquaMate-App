d709b5d08da45031648ca42bc5d8590344a2d7a1
AndresQuev <andresquev4@gmail.com>
2025-11-28 17:01:42 -0300

    New User Profile view

---
+++ AquaMate/Views/Home/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  AquaMate
+//
+//  Created by Andrés Quevedo on 28/11/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
